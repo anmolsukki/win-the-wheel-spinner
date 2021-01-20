635f390903b8bcf769b9be2e50ed34c85169e6a3
anmol <anmol.s@gyftr.com>
2021-01-20 12:31:34 +0530

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
 
-function App() {
+const App = () => {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const prize = ['PRIZE 1', 'PRIZE 2', 'PRIZE 3', 'PRIZE 4', 'PRIZE 5', 'PRIZE 6', 'PRIZE 7', 'PRIZE 8'];
+  const spinning = selectedItem !== null ? 'spinning' : '';
+  const wheelVars = { '--nb-item': prize.length, '--selected-item': selectedItem };
+
+  const startSpin = () => {
+    if (selectedItem === null) {
+      const roundSelected = Math.floor(Math.random() * prize.length);
+      setSelectedItem(roundSelected);
+    } else {
+      setSelectedItem(null);
+      setTimeout(this.selectItem, 500);
+    }
+  };
+
+  useEffect(() => {
+    if (selectedItem) {
+      setTimeout(() => {
+        alert('You have won PRIZE ' + parseInt(selectedItem + 1));
+      }, 1500);
+    }
+  }, [selectedItem])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="col-12 col-lg-12 text-center text-lg-right">
+        <div className="wheel-container">
+          <div className={`wheel ${spinning}`} style={wheelVars}>
+            {prize.map((item, index) => (
+              <div className="wheel-item" key={index} style={{ '--item-nb': index }}>
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+        <button id="spin_button" onClick={() => startSpin()}>
+          TAP TO SPIN
+        </button>
+      </div>
     </div>
   );
 }
